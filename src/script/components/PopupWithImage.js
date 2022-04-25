ee97ed437225f461a2f6a5d588da44528d41371b
ob <oberezhinskiy@gmail.com>
2022-04-25 14:18:51 +0300

    fix PopupWithImage

--- src/script/components/PopupWithImage.js
+++ src/script/components/PopupWithImage.js
@@ -1,17 +1,18 @@
 import Popup from "./Popup"
 
-  export default class PopupWithImage extends Popup {
-   constructor(popupSelector,link,name ) {
-     super(popupSelector,link,name);
-     this._link = link
-     this._name = name
-   }
-   setImgPopup(){
-    const popupImgScale = document.querySelector(".popup__img");
-    const popupTittleScale = document.querySelector(".popup__img-title");
-    popupImgScale.src = this._link;
-    popupImgScale.alt = this._name;
-    popupTittleScale.textContent = this._name;
-   }
+export default class PopupWithImage extends Popup {
+  constructor(popupSelector, link, name) {
+    super(popupSelector, link, name);
+    this._link = link
+    this._name = name
+    this._popupImgScale = document.querySelector(".popup__img");
+    this._popupTittleScale = document.querySelector(".popup__img-title");
+  }
+  setImgPopup() {
+
+    this._popupImgScale.src = this._link;
+    this._popupImgScale.alt = this._name;
+    this._popupTittleScale.textContent = this._name;
+  }
 }
 
